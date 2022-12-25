55ec4dcf45e2f65d1e0b29170fd8799698ab9b85
Noraiz123 <gnoraiz512@gmail.com>
2022-12-25 13:07:54 +0500

    production api url added

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:8000/';
-// export const BASE_URL = 'https://konfor-backend.herokuapp.com/';
+// export const BASE_URL = 'http://localhost:8000/';
+export const BASE_URL = 'https://store-pos-backend.onrender.com/';
