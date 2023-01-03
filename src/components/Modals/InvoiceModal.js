5a1d02cf0dac7f97dbe3a940a3e564fe404eee4d
Noraiz123 <gnoraiz512@gmail.com>
2023-01-03 22:50:05 +0500

    invoice issue fixed

--- src/components/Modals/InvoiceModal.js
+++ src/components/Modals/InvoiceModal.js
@@ -20,18 +20,18 @@ const InvoiceModal = ({ isOpen, setIsOpen, invoiceData }) => {
             Invoice
           </Dialog.Title>
           <div className='h-50v overflow-y-auto main'>
-            <div className='ticket bg-white border-2 border-dotted text-sm' ref={invoiceRef}>
-              <h1 className='text-center font-extrabold text-xl mt-2'>Al Hamd Kiryana Store</h1>
+            <div className='ticket p-3 bg-white border-2 border-dotted text-sm' ref={invoiceRef}>
+              <h1 className='text-center font-extrabold text-xl text-black mt-2'>Al Hamd Kiryana Store</h1>
               <div className='flex px-3 my-2'>
                 <div>
                   <div>
-                    <address className='text-sm'>
+                    <address className='text-black'>
                       <span className='font-bold'> Order Date : </span>
                       {new Date().toLocaleDateString()}
                     </address>
                   </div>
                   <div>
-                    <address className='text-sm'>
+                    <address className='text-black'>
                       <span className='font-bold'> Invoice# : </span>
                       {invoiceData?.invoiceNo || 'N/A'}
                     </address>
@@ -39,46 +39,47 @@ const InvoiceModal = ({ isOpen, setIsOpen, invoiceData }) => {
                 </div>
                 <div>
                   <div>
-                    <address className='text-sm'>
+                    <address className='text-black'>
                       <span className='font-bold'>Customer Name : </span>
                       {invoiceData?.customer?.name ? invoiceData.customer.name : 'N/A'}
                     </address>
                   </div>
                   <div>
-                    <address className='text-sm'>
+                    <address className='text-black'>
                       <span className='font-bold'>Customer Phone : </span>
                       {invoiceData?.customer?.phone_no ? invoiceData.customer.phone_no : 'N/A'}
                     </address>
                   </div>
                 </div>
               </div>
-              <table className='mx-auto text-sm w-full invoice-table'>
+              <table className='mx-auto font-bold w-full invoice-table'>
                 <thead>
-                  <tr className=''>
-                    <th className='py-1 text-left'>Name</th>
-                    <th className='py-1 text-left'>Qty</th>
+                  <tr className='text-black'>
                     <th className='py-1 text-left'>$$</th>
+                    <th className='py-1 text-left'>Qty</th>
+                    <th className='py-1 text-left'>Name</th>
                   </tr>
                 </thead>
                 <tbody>
                   {invoiceData.orderItems &&
                     invoiceData.orderItems.map((e, i) => (
-                      <tr className='text-gray-600 invoice-data' key={e.id}>
-                        <td className='py-1 text-left'>{e.name}</td>
-                        <td className='py-1 text-left'>{e.orderQuantity}</td>
+                      <tr className='text-black invoice-data' key={e.id}>
                         <td className='py-1 text-left'>
                           {Math.round((e.price - (e.price * e.discount) / 100) * e.orderQuantity)}
                         </td>
+                        <td className='py-1 text-left'>{e.orderQuantity}</td>
+
+                        <td className='py-1 text-left'>{e.name}</td>
                       </tr>
                     ))}
-                  <tr className='mx-auto invoice-total'>
+                  <tr className='mx-auto text-black invoice-total'>
                     <td></td>
                     <td>TOTAL</td>
                     <td>{Math.round(invoiceData.total)}</td>
                   </tr>
                 </tbody>
               </table>
-              <p className='text-center mb-20'>Thanks for your purchase!</p>
+              <p className='text-center text-black'>Thanks for your purchase!</p>
             </div>
           </div>
 
